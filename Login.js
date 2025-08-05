a1505d662a41fb0f2de7c68259d956e6523ccb4b
CLINTON MORSE <CLINTON1972@OUTLOOK.COM>
2025-08-05 17:01:03 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,10 +1,14 @@
-export default function Login({ onLogin }) {
-  return (
-    <div>
-      <h1>Login to VORTEX.X</h1>
-      <button onClick={() => onLogin({ name: 'DemoUser', plan: 'Pro' })}>
-        Login as Demo
-      </button>
-    </div>
-  );
-}
+import { verifyUserPlan } from './PlanVerifier.js';
+
+...
+
+<button
+  onClick={async () => {
+    const email = prompt('Enter your email address to log in');
+    if (!email) return;
+    const verifiedPlan = await verifyUserPlan(email);
+    onLogin({ name: email.split('@')[0], plan: verifiedPlan });
+  }}
+>
+  Login as Demo
+</button>
